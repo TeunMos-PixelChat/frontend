bfdaa4d0823b715e175bb8f006beebc9de05cca1
teunmos <teun.mos@gmail.com>
2024-04-03 11:16:28 +0200

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
 import React from 'react';
-// import Shell from './components/shell/shell';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shell from './components/shell/shell';
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
 
 import TestPage from './pages/testpage';
-// import InnerHeader from './components/shell/innerHeader';
+import InnerHeader from './components/shell/innerHeader';
 import { Center } from '@mantine/core';
 
 function App() {
-
-  // const currentPath = window.location.pathname
+  const currentPath = window.location.pathname
 
 
   return (
-    // <Shell path={currentPath}>
       <BrowserRouter>
-      <Routes>
-        <Route index element = {
-            // <InnerHeader>
-              <TestPage />
-            // </InnerHeader>
-        } />
-        <Route path="*" element = {
-            <Center>Not found</Center>
-        } />
-      </Routes>
-    </BrowserRouter>
-    // </Shell>
+        <Shell path={currentPath}>
+          <Routes>
+            <Route index element = {
+                <InnerHeader>
+                  <TestPage />
+                </InnerHeader>
+            } />
+            <Route path="*" element = {
+                <Center>Not found</Center>
+            } />
+          </Routes>
+        </Shell>
+      </BrowserRouter>
   );
 }
 
--- src/components/shell/smallSideBarButtons.tsx
+++ src/components/shell/smallSideBarButtons.tsx
@@ -1,30 +1,71 @@
-import { ActionIcon, Center, Stack } from '@mantine/core';
-import styles from './smallSideBar.module.css';
-import GoogleIcon from '../googleIcon';
+import React, { useEffect, useState } from "react";
+import { ActionIcon, Center, Stack } from "@mantine/core";
+import styles from "./smallSideBar.module.css";
+import GoogleIcon from "../googleIcon";
+import { useNavigate, useLocation } from "react-router-dom";
 
-export default function SmallSidebarButtons({ path }: { path: string }) {
+export type navButton = "home" | "search" | "notifications";
 
-  function navigateTo(path: string) {
-    window.location.pathname = path;
+export default function SmallSidebarButtons() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [activatedButton, setActivatedButton] = useState<navButton>("home");
+
+
+  useEffect(() => {
+    const path = location.pathname.split("/")[1] ?? "";
+    if (path === "") {
+      setActivatedButton("home");
+    } else {
+      setActivatedButton(path as navButton);
+    }
+    
+  }, [location]);
+
+  function navigateTo(path: string, button: navButton) {
+    setActivatedButton(button);
+    navigate(path);
   }
-
 
   return (
     <Center className={styles.container}>
       <Stack>
-        <SidebarButton icon="home" active={ path === '/' } onClick={() => navigateTo('/')} />
-        <SidebarButton icon="search" active={ path.startsWith('/search') } onClick={() => navigateTo('/search')} />
-        <SidebarButton icon="notifications" active={ path.startsWith('/notifications') } onClick={() => navigateTo('/notifications')} />
+        <SidebarButton
+          icon="home"
+          active={activatedButton === "home"}
+          onClick={() => navigateTo("/", "home")}
+        />
+        <SidebarButton
+          icon="search"
+          active={activatedButton === "search"}
+          onClick={() => navigateTo("/search", "search")}
+        />
+        <SidebarButton
+          icon="notifications"
+          active={activatedButton === "notifications"}
+          onClick={() => navigateTo("/notifications", "notifications")}
+        />
       </Stack>
     </Center>
-    
   );
 }
 
-function SidebarButton({ icon, active, onClick}: { icon: string; active: boolean; onClick?: () => void }) {
+function SidebarButton({
+  icon,
+  active,
+  onClick,
+}: {
+  icon: string;
+  active: boolean;
+  onClick?: () => void;
+}) {
   return (
-    <ActionIcon variant={active ? 'default' : 'transparent'} size="lg" onClick={onClick}>
-      <GoogleIcon icon={icon} size={20} color={'var(--mantine-color-text)'} />
+    <ActionIcon
+      variant={active ? "default" : "transparent"}
+      size="lg"
+      onClick={onClick}
+    >
+      <GoogleIcon icon={icon} size={20} color={"var(--mantine-color-text)"} />
     </ActionIcon>
   );
 }
