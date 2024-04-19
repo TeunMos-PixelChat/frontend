7c5ab8399b263712fea1ffa07192c7f9bc8d70d8
teunmos <teun.mos@gmail.com>
2024-04-19 10:51:12 +0200

    test

--- src/pages/testPage.tsx
+++ src/pages/testPage.tsx
@@ -91,6 +91,7 @@ export default function TestPage() {
             </Button>
             <Stack>
               <Code>Env: {process.env.NODE_ENV}</Code>
+              <Code>Test</Code>
             </Stack>
 
 
